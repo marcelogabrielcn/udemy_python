fc97cc7d9dbf258097e541c37c83c1470021864f
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2023-01-23 21:15:32 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,16 @@
+# Aspas simples
+print('Marcelo Gabriel')
+
+# Aspas duplas
+print("Marcelo Gabriel")
+
+
+# Escape
+print("Marcelo \"Gabriel\"")
+
+# r
+print(r"Marcelo \"Gabriel\"")
+
+
+# Mais simples de se usar
+print('Marcelo"Gabriel"')
