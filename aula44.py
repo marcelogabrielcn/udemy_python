dcf91c20a802f99b685a16fcbbaf9e2972465e80
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2023-02-14 16:54:25 -0300

    Gerador de CPF
    Ajustes de código

--- aula44.py
+++ aula44.py
@@ -9,7 +9,6 @@ for n in range(9):
     primeiros_9_digitos += str(random.randint(0, 9))
 
 cpf += primeiros_9_digitos
-print(primeiros_9_digitos)
 
 num_calc = 10
 soma_9_prim_digit = 0
@@ -26,7 +25,6 @@ for n in cpf:
 check_d1 = soma_9_prim_digit * 10
 check_d1 = check_d1 % 11
 d1 = check_d1 if check_d1 <= 9 else 0
-print(f'O primeiro dígito é {d1}')
 
 cpf += str(d1)
 
@@ -45,8 +43,6 @@ for n in cpf:
 check_d2 = (soma_10_prim_digit * 10) % 11
 d2 = 0 if check_d2 > 9 else check_d2
 
-print(f'O segundo dígito é {d2}')
 cpf += str(d2)
 
-print(cpf)
-
+print(f'CPF gerado: {cpf}')
