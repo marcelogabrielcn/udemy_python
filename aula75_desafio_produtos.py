1bf5534c863fd986edbcd482a4ac6fc1264d74d3
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2023-03-16 22:19:38 -0300

    Add files via upload

--- aula75_desafio_produtos.py
+++ aula75_desafio_produtos.py
@@ -1,3 +1,5 @@
+import copy
+
 # *Ver aula 160 no curso da Udemy com proposta do desafio
 
 produtos = [
@@ -12,10 +14,30 @@ produtos_com_aumento = []
 
 for item in produtos:
     dict_temp = {
-        'nome': item['nome'], 'preco': (item['preco'] + (item['preco']/100) * 10)
+        'nome': item['nome'], 'preco': round((item['preco'] + (item['preco']/100) * 10), 2)
     }
     produtos_com_aumento.append(dict_temp)
-    print(produtos_com_aumento)
+
     #produtos_com_aumento.append(item['nome'])
     #produtos_com_aumento.append(float(item['preco']) + (float(item['preco'])/100) * 10)
     #print(produtos_com_aumento)
+
+#for produtos in produtos:
+#   print(produtos)
+print('Produtos originais')
+print(*produtos, sep='\n')
+
+#for produtos in produtos_com_aumento:
+#    print(produtos)
+print('\nProdutos com alteração de preço')
+print(*produtos_com_aumento, sep='\n')
+
+produtos_ordenados_por_nome = sorted(copy.deepcopy(produtos_com_aumento), key= lambda p: p['nome'], reverse=True)
+
+print('\nProdutos ordenados por nome, em ordem decrescente')
+print(*produtos_ordenados_por_nome, sep='\n')
+
+produtos_ordenados_por_preco = sorted(copy.deepcopy(produtos_com_aumento), key= lambda p: p['preco'])
+
+print('\nProdutos ordenados por preço')
+print(*produtos_ordenados_por_preco, sep='\n')
