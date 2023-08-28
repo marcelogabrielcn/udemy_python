0cde3534841f9d48b41ba90248210311954e0927
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2023-08-28 20:29:48 -0300

    Add files via upload

--- aula92_exercicio.py
+++ aula92_exercicio.py
@@ -1,23 +1,60 @@
 # Criar uma lista de tarefas, onde o usuário pode criar uma tarefa ou listar. Pode desfazer o ultimo comando ou refazer o que havia desfeito
 
-lista_de_tarefas = []
-app = True
+def listar(lista_de_tarefas):
+    print()
+    if not lista_de_tarefas:
+        print('Não há nenhuma tarefa para listar.')
+        return
 
-while app == True:
+    print('Tarefas:')
+    for tarefa in lista_de_tarefas:
+        print(f'\t{tarefa}')
+
+
+def desfazer(lista_de_tarefas, tarefas_refazer):
+    print()
+    if not lista_de_tarefas:
+        print('Não há nenhuma tarefa para desfazer.')
+        return
+    
+    tarefa = lista_de_tarefas.pop()
+    tarefas_refazer.append(tarefa)
+
+
+def refazer(lista_de_tarefas, tarefas_refazer):
+    print()
+    if not tarefas_refazer:
+        print('Não há nenhuma tarefa para refazer.')
+        return
+    
+    tarefa = tarefas_refazer.pop()
+    lista_de_tarefas.append(tarefa)
+
+
+def adicionar_tarefa(tarefa, lista_de_tarefas):
+    print()
+    if not tarefa:
+        print('Ops, parece que você não digitou nenhuma tarefa.')
+        return
+    lista_de_tarefas.append(tarefa)
+    listar(lista_de_tarefas)
+
+lista_de_tarefas = []
+tarefas_refazer = []
+
+while True:
     acao_usuario = input(str('Comandos: listar, desfazer, refazer, sair. \nDigite uma tarefa ou comando: '))
     if acao_usuario == 'listar':
-        for tarefas in lista_de_tarefas:
-            print(tarefas)
+        listar(lista_de_tarefas)
     elif acao_usuario == 'desfazer':
-        ...
+        desfazer(lista_de_tarefas, tarefas_refazer)
     elif acao_usuario == 'refazer':
-        ...
+        refazer(lista_de_tarefas, tarefas_refazer)
     elif acao_usuario == 'sair':
-        app = False
+        break
     else:
-        lista_de_tarefas.append(acao_usuario)
-        for tarefas in lista_de_tarefas:    
-            print(tarefas)
+        adicionar_tarefa(acao_usuario.strip(), lista_de_tarefas)
+
 
 print('Obrigado por utlizar o Tarefy. Volte sempre!❤️')
 
