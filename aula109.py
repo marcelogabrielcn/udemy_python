6ff2e102967cfdb8919a7661656c8ae1a845d4f3
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2024-06-17 22:26:47 -0300

    Add files via upload

---
+++ aula109.py
@@ -0,0 +1,6 @@
+# Será feito um diretório só para esta aula, verificar pasta com mesmo nome deste arquivo
+# 
+# Parte 1 - Mixins, Abstração
+# Parte 2 - Log, LogFileMixin, LogPrintMixin
+# Parte 3 - LogFileMixin
+# Parte 4 - Eletrônicos, Smartphone com Mixin
