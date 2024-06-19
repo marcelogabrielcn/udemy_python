1ae6a762cfc32632371a3be664999f906a8e3e11
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2024-06-19 20:33:46 -0300

    Add files via upload

--- aula109/log.py
+++ aula109/log.py
@@ -1,16 +1,27 @@
 # Abstração
 
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o método Log')
     
 
-class LogFileMixin:
-    def log(self, msg):
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+    
+
+class LogFileMixin(Log):
+    def _log(self, msg):
         print(msg)
 
 
-if __name__ == '__main__':
-    l = Log()
-    l.log('Mensagem qualquer')
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} {self.__class__.__name__}')
 
+
+if __name__ == '__main__':
+    l = LogPrintMixin()
+    l.log_error('Mensagem qualquer')
