004937a823bb325f4042543852b8d59f18d70754
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-07-02 20:31:42 -0300

    aula227

--- aula109/log.py
+++ aula109/log.py
@@ -1,4 +1,9 @@
 # Abstração
+from pathlib import Path
+
+log_file = Path(__file__).parent / 'log.txt'  # Indica que é para criar um aquivo com este nome, e salvar lá.
+print(f'Caminho do arquivo: {log_file}')  # Retorna o caminho do arquivo, qual diretóierio esse arquivo está
+
 
 class Log:
     def _log(self, msg):
@@ -14,7 +19,11 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no arquivo de log')
+        with open(log_file, 'a') as arquivo:  # open com 'a' ele adiciona linhas, com 'w' ele apaga e começa do 0
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
 
 
 class LogPrintMixin(Log):
@@ -25,4 +34,9 @@ class LogPrintMixin(Log):
 if __name__ == '__main__':
     l = LogPrintMixin()
     l.log_error('Mensagem qualquer')
+    l.log_success('Eba, deu certo')
+
+    l2 = LogFileMixin()
+    l2.log_error('teste mixin')
+    l2.log_success('uhuul')
 
--- aula109/main.py
+++ aula109/main.py
@@ -1,2 +1,10 @@
-from log import Log
+from log import LogFileMixin, LogPrintMixin
 
+
+l = LogPrintMixin()
+l.log_error('Mensagem qualquer')
+l.log_success('Eba, deu certo')
+
+l2 = LogFileMixin()
+l2.log_error('Adicionando linhas com log')
+l2.log_success('adicionando linhas, success')
