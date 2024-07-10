38a4067e81af3e81e6c85a072ce6936a062b69a7
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-07-10 20:53:54 -0300

    aula 230_2

--- aula111_abstractmethod_property-setter.py
+++ aula111_abstractmethod_property-setter.py
@@ -7,22 +7,22 @@ class AbstractFoo(ABC):  # *É uma classe abstrata, ou seja, não é para usar, 
         self.name = name
 
     @property
+
+    def name(self):
+        return self._name
+    
+    @name.setter
     @abstractmethod
     def name(self):
         return self._name
-
+    
 
 class Foo(AbstractFoo):
     def __init__(self, name):
         super().__init__(name)
         print("Não sou útil nesse código")  # Esse parte é inútil porque o super apenas passa o código para classe pai
 
-
-    @property
-    def name(self):
-        return self._name
-    
-    @name.setter
+    @AbstractFoo.name.setter
     def name(self, name):
         self._name = name
 
