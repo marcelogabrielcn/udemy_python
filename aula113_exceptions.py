331d63a592bf77fc94b5ef44cf9724f597b7e3f6
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-07-16 21:28:46 -0300

    aula235

--- aula113_exceptions.py
+++ aula113_exceptions.py
@@ -1,11 +1,19 @@
 class MeuError(Exception): 
     ...
+    
 
+class OutroError(Exception): ...
 
 def levantarException():
-    raise MeuError('A mensagem de erro vem aqui')  # Esse raie vai levantar um erro, e mostrar a mensagem ao usuário
+    exception_ = MeuError('A mensagem de erro vem aqui')  # Esse raie vai levantar um erro, e mostrar a mensagem ao usuário
+    exception_.add_note('Aqui posso adicionar uma nota e explicar o erro')
+    raise exception_
 
 
-levantarException()
-
-
+try:
+    levantarException()
+except (MeuError, ZeroDivisionError) as error:  # Se algum tipo e exceção que estiver aqui em except ocorrer durante o code, vai entrar nessa clausula
+    print(error.__class__.__name__)
+    print()
+    exception_ = OutroError('Será lançado de novo o erro')
+    raise exception_ from error
