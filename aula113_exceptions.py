b3262c434e9dcaff981a1cb677e19f2fd783b6e8
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-07-16 21:14:34 -0300

    aula234

--- aula113_exceptions.py
+++ aula113_exceptions.py
@@ -3,7 +3,9 @@ class MeuError(Exception):
 
 
 def levantarException():
-    raise MeuError('A mensagem de erro vem aqui')  # Esse raie vai levantar um erro, e mostrar ao usuário
+    raise MeuError('A mensagem de erro vem aqui')  # Esse raie vai levantar um erro, e mostrar a mensagem ao usuário
 
 
 levantarException()
+
+
