a632e4ee95aac4e8f9e08e84cf39642c161932cd
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-07-18 21:49:45 -0300

    aula 237

---
+++ aula114_dunder-methods.py
@@ -0,0 +1,19 @@
+class Ponto:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        class_name1 = self.__class__.__name__
+        class_name2 = type(self).__name__
+        return f'{class_name1, class_name2}(x={self.x}, y={self.y})' 
+
+
+p1 = Ponto(1, 2)
+p2 = Ponto(293, 421)
+print(p1)
+print(p2)
+print()
+print(repr(p2))
+print(f'{p2!s}')
+print(f'{p1!r}')
