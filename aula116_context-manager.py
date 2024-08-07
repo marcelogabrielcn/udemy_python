d51925d8ffe45d89d842d385a6dc6797b88e69ca
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-08-06 21:46:31 -0300

    Aula 242

--- aula116_context-manager.py
+++ aula116_context-manager.py
@@ -17,7 +17,15 @@ class MyOpen:
 
     def __exit__(self, class_exception, exception_, traceback_):
         print('Fechando arquivo')
-        return self._arquivo.close()
+        self._arquivo.close()
+
+        #raise class_exception('Minha mensagem de Exceção')  # Assim eu posso levantar minha exceção, porém, o raise anula o ruturn
+
+        print(class_exception)
+        print(exception_)
+        print(traceback_)
+
+        return True  # Jã tratei o erro de alguma forma, dessa maneira o erro apenas irá passar.
 
 
 with MyOpen('aula116.txt', 'w', ) as arquivo:
