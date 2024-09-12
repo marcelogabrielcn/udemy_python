fb0dc185b896e2e649631648f2117dfee1ca7233
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-09-12 18:17:48 -0300

    aula 121 part II

--- aula121_Documentation/uma_linha.py
+++ aula121_Documentation/uma_linha.py
@@ -1,6 +1,9 @@
 """
 Documentação do módulo 
-Aqui vem o __name__, seria descrever o que o módulo faz
+Aqui vem o __name__, que deve ser o nome do módulo e título, algo assim
+
+Se eu pular uma linha, e adicionar texto, aqui será a descrição que irá aparecer no help()
+Independente de quantas linhas tiver, irá aparecer aqui como descrição
 """
 
 
