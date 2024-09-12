4e93eec69175238446f5b12e641a0111abb5f07d
marcelogabrielcn <marcelogabrielcn@gmail.com>
2024-09-12 18:09:40 -0300

    aula 121

---
+++ aula121_Documentation/main.py
@@ -0,0 +1,16 @@
+import uma_linha
+
+print(dir(uma_linha))
+# O Dir() vai mostar tudo que tem dentro do módulo
+
+print(uma_linha.__doc__)
+# retorna a documentação do meu arquivo, se tiver
+
+print(uma_linha.__file__)
+# retorna o caminho do meu arquivo
+
+print(uma_linha.__name__)
+# Retornar o nome do meu módulo
+
+help(uma_linha)
+#Da uma "Ajuda" sobre tudo que tem no módulo, todas as funções acima serão executadas, a ideia é começar preencher esses docs agora
---
+++ aula121_Documentation/uma_linha.py
@@ -0,0 +1,11 @@
+"""
+Documentação do módulo 
+Aqui vem o __name__, seria descrever o que o módulo faz
+"""
+
+
+print('Oii')
+var_qualquer = 'nada'
+
+def func_qualquer():
+    ...
