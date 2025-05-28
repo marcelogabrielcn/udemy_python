41e602f1014e89b14d0a92e1d0e4b805d9fc7e87
marcelogabrielcn <marcelogabrielcn@gmail.com>
2025-05-27 21:39:15 -0300

    20250507

--- aula123_sistema_bancario/contas.py
+++ aula123_sistema_bancario/contas.py
@@ -9,11 +9,30 @@ class Conta(abc.ABC):
     @abc.abstractclassmethod
     def sacar(self, valor):
         self.valor = valor
-        self.detalhes(f'(Saque de R${valor:.2f}!)')
+        self.detalhes(f'(Saque de R${valor:.2f})')
 
     def depositar(self, valor):
         self.saldo += valor
-        self.detalhes(f'(Deposito de R${valor:.2f}!)')
+        self.detalhes(f'(Deposito de R${valor:.2f})')
 
     def detalhes(self, msg=''):
         print(f'O seu saldo é {self.saldo:.2f} {msg}')
+        print('----------------------------------------')
+
+class ContaPopanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(Saque {valor})')
+            return self.saldo
+        
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(Saque Negado {valor})')
+
+if __name__ == '__main__':
+    cp1 = ContaPopanca(3333, 1029, 0)
+    cp1.sacar(1)
+    cp1.depositar(1923)
+    cp1.sacar(25)
