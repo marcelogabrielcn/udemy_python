7434ec4ca7475f0eb39805502a5c82cd460e1204
marcelogabrielcn <marcelogabrielcn@gmail.com>
2025-07-21 22:02:26 -0300

    20250721

--- aula123_sistema_bancario/contas.py
+++ aula123_sistema_bancario/contas.py
@@ -8,8 +8,7 @@ class Conta(abc.ABC):
     
     @abc.abstractclassmethod
     def sacar(self, valor):
-        self.valor = valor
-        self.detalhes(f'(Saque de R${valor:.2f})')
+        ...
 
     def depositar(self, valor):
         self.saldo += valor
@@ -31,8 +30,30 @@ class ContaPopanca(Conta):
         print('Não foi possível sacar o valor desejado')
         self.detalhes(f'(Saque Negado {valor})')
 
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo=0, limite=0):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+    
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo = -self.limite
+
+        if valor_pos_saque >= limite_maximo:
+            self.saldo -= valor
+            self.detalhes(f'(Saque {valor})')
+            return self.saldo
+
+
 if __name__ == '__main__':
-    cp1 = ContaPopanca(3333, 1029, 0)
-    cp1.sacar(1)
-    cp1.depositar(1923)
-    cp1.sacar(25)
+    cp1 = ContaPopanca(1111, 1029, 0)
+    cp1.sacar(10)
+    cp1.depositar(200)
+    cp1.sacar(20)
+    print('##')
+
+    cp2 = ContaCorrente(2222, 3847, 0, 1000)
+    cp2.sacar(10)
+    cp2.depositar(2000)
+    cp2.sacar(20)
+    print('##')
